4a632a72a5acd21376be4233a7124d55cab60de2
Brayan Reynoso Macedo <20213tn084@utez.edu.mx>
2025-01-06 10:25:44 -0600

    [Update] implement infinite scrolling in OffersCarousel with seamless transitions

--- brew_station_ui/lib/widgets/home/offers_carousel.dart
+++ brew_station_ui/lib/widgets/home/offers_carousel.dart
@@ -1,5 +1,6 @@
-import 'package:brew_station_ui/core/constants/colors.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:brew_station_ui/core/constants/colors.dart';
 
 class Offer {
   final String text;
@@ -8,8 +9,14 @@ class Offer {
   Offer({required this.text, required this.image});
 }
 
-class OffersCarousel extends StatelessWidget {
-  // Lista de ofertas, cada una con su texto y su imagen correspondiente
+class OffersCarousel extends StatefulWidget {
+  const OffersCarousel({super.key});
+
+  @override
+  _OffersCarouselState createState() => _OffersCarouselState();
+}
+
+class _OffersCarouselState extends State<OffersCarousel> {
   final List<Offer> offers = [
     Offer(text: 'Oferta 1: Descuento del 20% en café', image: 'assets/images/offer.jpg'),
     Offer(text: 'Oferta 2: Compra uno, lleva uno gratis', image: 'assets/images/offer2.jpg'),
@@ -17,12 +24,44 @@ class OffersCarousel extends StatelessWidget {
     Offer(text: 'Oferta 4: Envío gratis en compras mayores a 100', image: 'assets/images/offer4.webp'),
   ];
 
+  late PageController _pageController;
+  late Timer _timer;
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 0);
+
+    // Configurar el temporizador para cambiar de página automáticamente
+    _timer = Timer.periodic(const Duration(seconds: 3), (timer) {
+      if (_currentPage < offers.length - 1) {
+        _currentPage++;
+      } else {
+        _currentPage = 0; // Volver al inicio si estamos en la última página
+      }
+      _pageController.animateToPage(
+        _currentPage,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeInOut,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel(); // Cancelar el temporizador al salir del widget
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 180,
       width: 400,
       child: PageView.builder(
+        controller: _pageController,
         itemCount: offers.length,
         itemBuilder: (context, index) {
           final offer = offers[index];
@@ -38,11 +77,9 @@ class OffersCarousel extends StatelessWidget {
                   image: AssetImage(offer.image),
                   fit: BoxFit.cover,
                 ),
-                
               ),
               child: Stack(
                 children: [
-                  // Badge "Promo"
                   Positioned(
                     top: 10,
                     left: 10,
@@ -67,10 +104,10 @@ class OffersCarousel extends StatelessWidget {
                       offer.text,
                       textAlign: TextAlign.center,
                       style: const TextStyle(
-                       color: AppColors.white,
+                        color: AppColors.white,
                         fontSize: 18,
                         fontWeight: FontWeight.bold,
-                        fontFamily: "sora"
+                        fontFamily: "sora",
                       ),
                     ),
                   ),
